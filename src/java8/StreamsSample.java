fb12157a6689abbcf7c844af9b777b31dcdf5fe9
bugsdeepak <antonyb.deepak@gmail.com>
2025-02-20 16:27:07 -0600

    target folder removed and new Stream methods added

--- src/java8/StreamsSample.java
+++ src/java8/StreamsSample.java
@@ -1,8 +1,26 @@
 package java8;
 
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
+
+/**
+ * Debugging Java streams directly using traditional breakpoints can be challenging due to their lazy and functional nature.
+ * however there are some alternate approaches
+ *
+ * 1. Using peek() for Intermediate Inspection
+ * 2. Stream Debugger in IDEs - IntelliJ IDEA
+ *
+ * Streams are functional in nature, meaning that operations on a stream produce a new stream without modifying the source.
+ * This allows for efficient and potentially parallel processing of data.
+ *
+ */
 public class StreamsSample {
 
 	public static int f(int n) {
@@ -36,7 +54,47 @@ public class StreamsSample {
 		
 		System.out.println("Result = " + result);
 
-		
+		/**
+		 * 	 * Types of Functional Interface
+		 * 	 * 1. Predicate - Predicate<T> { boolean test (T t) } - removeIf(), filter() is a Predicate
+		 * 	 * 2. Function -  Function<T, R> { R apply(T t) } - sort(Comparator comp)
+		 * 	 * 3. Bi-Function - BiFunction<T, U, R> { R apply (T t, U u) } - Map.replaceAll(K, V)
+		 * 	 * 4. Supplier - Supplier<T> { T get() } - Use Optional - NULL OBJECT PATTERN
+		 * 	 * 5. Consumer - Consumer<T> { void accept(T t) }  - forEach() is a Consumer
+		 */
+
+		//1. Predicate
+		List<Integer> integerList = List.of(12, 123, 23, 234, 20);
+		//Returns true if the input argument matches the predicate, otherwise false
+		Predicate<Integer> isOdd  =  x -> x % 2 == 1;
+
+		List<Integer> oddList = integerList.stream().filter(isOdd).toList();
+		System.out.println("Predicate Exmple: All Odd Number is the list = " + oddList.toString());
+
+		//2. Function
+		// A function that return the last name when two names separated by space
+		Function<String, String> getLastName = (x) -> {
+															String items [] = x.split(" ");
+		                                                    return items[1];
+															};
+		final String fullName = "Antony Benedict";
+		System.out.println("Antony Benedict's last name = " + getLastName.apply(fullName));
+		Function<String, Integer> findLength = x -> x.length();
+		//Chaining Functions
+		System.out.println("Antony Benedict's Last Name Length = " + getLastName.andThen(findLength).apply(fullName));
+
+		//3. Bi-Function
+		//BiFunction<T, U, R>, where T is the type of the first argument, U is the type of the second argument, and R is the type of the result.
+
+		// Example 1: Adding two integers
+		BiFunction<Integer, Integer, Integer> add = (a, b) -> a + b;
+		int sum = add.apply(5, 3); // sum will be 8
+		System.out.println("Sum: " + sum);
+
+		// Example 2: Concatenating two strings
+		BiFunction<String, String, String> concat = (s1, s2) -> s1 + s2;
+		String resultStr = concat.apply("Hello, ", "World!"); // result will be "Hello, World!"
+		System.out.println("Concatenation: " + resultStr);
 	}
 
 }
